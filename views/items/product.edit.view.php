9727910ac4e48427fb70905ea4ffdd8e58663df3
penh sovannarith <sovannarith.penh@student.passerellesnumeriques.org>
2024-02-21 19:10:02 +0700

    [feat] change interface product ZX15-36

--- views/items/product.edit.view.php
+++ views/items/product.edit.view.php
@@ -2,50 +2,74 @@
 require "layouts/header.php";
 require "layouts/navbar.php";
 ?>
-<div class="m-4 ">
-    <form action = "controllers/items/item.update.controller.php" method = "post">
+<div class="m-4">
+    <form action="controllers/items/item.update.controller.php" method="post"
+        class="container col-6 p-3 mb-2 bg-primary">
         <input type="hidden" name="id" value="<?=$product['id']?>">
-        <h1 class="bg-blue">Create Product</h1>
+        <h1 class="bg-blue m-0 font-weight-bold text-white text-center mb-3">Update Product</h1>
         <div>
-            <div class="form-group">
-                <label>Product Name:</label>
+            <div class="form-row ml-2">
+                <div class="form-group">
+                    <label class="text-white">Product Name:</label>
+                    <div>
+                        <input class="form-control col-md-11 mb-3 " type="text" name="name" placeholder="name"
+                            value="<?=$product['name']?>">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="text-white ml-1">Code of Product:</label>
+                    <div>
+                        <input class="form-control row-md-4 mb-3 mr-1" type="text" name="code" placeholder="$"
+                            value="<?=$product['price']?>">
+                    </div>
+                </div>
+                <div class="form-group d-flex">
+                    <div>
+                        <label class="text-white">Price:</label>
+                        <input class="form-control col-md-15 mb-1 " type="text" name="price" placeholder="$">
+                    </div>
+                    <div>
+                        <label class="text-white ml-4">Chose Image:</label>
+                        <input class="form-control col-10 ml-4" type="file" name="image" placeholder="image"
+                            value="<?=$product['price']?>">
+                    </div>
+                </div>
+      
+            </div>
+            
+            <div class="form-row ml-2">
+                <div class="form-group">
+                    <label class="text-white mt-4">Quantity:</label>
+                    <div>
+                        <input class="form-control col-md-19 mb-3" type="text" name="qty" placeholder="quantity"
+                            value="<?=$product['qty']?>">
+                    </div>
+                </div>
                 <div>
-                    <input class="form-control " type="text" name = "name" placeholder="name" value="<?=$product['name']?>">
+                    <label for="datemin" class="mt-4 text-white ml-2">Date of Product</label>
+                    <input class="form-control col-md-20 mb-18 ml-1 pb-30" type="date" id="datemin" name="date" min="2024-01-01"value=""><br>
                 </div>
             </div>
-            <div class="form-group">
-                <label>Price:</label>
-                <div>
-                    <input class="form-control" type="text" name = "price" placeholder="$" value="<?=$product['price']?>">
-                </div>
-            </div>
-            <div class="form-group">
-                <label>Quantity:</label>
-                <div>
-                    <input class="form-control" type="text" name = "qty" placeholder="quantity" value="<?=$product['qty']?>">
-                </div>
-            </div>
-            <select class="form-select form-select-sm" name = "category" aria-label=".form-select-sm example">
+            <select class="form-select form-select-sm ml-2" name="category" aria-label=".form-select-sm example">
                 <option selected disabled>Categories</option>
-               <?php foreach ($categories as $key => $category):?>
+                <?php foreach ($categories as $key => $category):?>
                 <option value="<?=$category['id']?>"><?=$category['categoryName']?></option>
                 <?php endforeach ?>
 
             </select>
-            <select class="form-select form-select-sm" name = "asign" aria-label=".form-select-sm example">
+            <select class="form-select form-select-sm" name="asign" aria-label=".form-select-sm example">
                 <option selected disabled>Asign Users</option>
                 <?php foreach ($users as $key => $user):?>
-                    <option value="<?=$user['id']?>"><?=$user['userName']?></option>
+                <option value="<?=$user['id']?>"><?=$user['userName']?></option>
                 <?php endforeach?>
             </select> <br>
-
-            <label for="datemin" class="mt-4">Date of Product</label>
-            <input type="date" id="datemin" name="date" min="2024-01-01" value=""><br>
             <div class="form-group d-flex justify-content-between mt-5">
-                <button type="submit" class="btn btn-primary">Update</button>
+                <button type="submit" class="btn btn-success">Update</button>
                 <a href="/items" class="btn btn-danger">Cancel</a>
 
             </div>
+
+
         </div>
     </form>
 
