4fa7ad286dca383b22fb6a8b089234e45c0fa918
thona ngon <thona ngon@example.com>
2024-03-12 18:44:18 +0700

    update navbar

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -50,18 +50,11 @@ $user = $_SESSION['user'];
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <a class="nav-link" href="/orders">
             <i class="fas fa-fw fa-shopping-cart"></i>
-            <span>Order</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Order Contains:</h6>
-                <a class="collapse-item" href="/orders">Add order</a>
-                <a class="collapse-item" href="/manage_order">Manage order</a>
-            </div>
-        </div>
+            <span>Order</span></a>
     </li>
+
     <!-- Nav Item - Tables -->
     <?php
     if ($user[5] === 'admin'):
