ce7cdbae4029a4ebd355b719bb9065b8ffc30bd4
thona ngon <thona ngon@example.com>
2024-03-25 20:40:44 +0700

    update interface reset password

--- views/forgotPassword/resetPassword.view.php
+++ views/forgotPassword/resetPassword.view.php
@@ -20,7 +20,7 @@ require "layouts/header.php";
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-none d-lg-block "><img src="../../assets/images/reset_password.png" alt="" style="width: 475px;height: 400px;"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
