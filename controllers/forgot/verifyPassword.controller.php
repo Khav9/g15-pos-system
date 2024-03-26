efecea4d896eea2a2a188cfecf841492bffe9002
KHAV.SAROEUN <KHAV.SAROEUN@PNC.LAN>
2024-03-26 20:53:39 +0700

    [imp] update on new password GPS-24

--- controllers/forgot/verifyPassword.controller.php
+++ controllers/forgot/verifyPassword.controller.php
@@ -5,12 +5,16 @@ $email = $_SESSION['emailOne'];
 if ($_SERVER['REQUEST_METHOD'] === "POST"){
       $password = htmlspecialchars($_POST['newPassword']);
       $confirmPassword = htmlspecialchars($_POST['confirmPassword']);
-      if ($password === $confirmPassword){
-            require '../../models/userManage.model.php';
-            $encryptPassword = password_hash($password, PASSWORD_BCRYPT);
-
-            updatePassword($email,$encryptPassword);
-            header('location: /');
+      if (!empty($password) && !empty($confirmPassword)){
+            if ($password === $confirmPassword){
+                  require '../../models/userManage.model.php';
+                  $encryptPassword = password_hash($password, PASSWORD_BCRYPT);
+      
+                  updatePassword($email,$encryptPassword);
+                  header('location: /');
+            }else{
+                  header('location: /newPassword');
+            }
       }else{
             header('location: /newPassword');
       }
